e67f66d93e081aaaf7fb579f31f7ac3c8934911b
Thalita <108842621+Euthalita@users.noreply.github.com>
2023-07-07 15:59:38 -0300

    Primeiro codigo POO

---
+++ POO/exemplos na pratica/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+		Garrafa c1 = new Garrafa();
+		//Caneta é a classe
+		//O c1 é o objeto criado
+		//O new é para instânciar o objeto
+		c1.cor = "Azul";
+		c1.tampada = false;
+		c1.volume = 0.5;
+		c1.status();
+		c1.comAgua();
+
+	}
+}
+
